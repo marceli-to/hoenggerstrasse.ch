87a612ba64539ee4aed5c9bebf35030cce0fd856
Marcel Stadelmann <m@marceli.to>
2024-04-08 20:26:48 +0200

    wip

--- resources/views/components/form/button.blade.php
+++ resources/views/components/form/button.blade.php
@@ -1,4 +1,4 @@
 @props(['type' => 'submit'])
-<button type="{{ $type }}" {{ $attributes->merge(['class' => 'bg-mist rounded-sm font-sans-bold font-bold flex text-navy uppercase py-10 px-20 hover:bg-white transition-all']) }}>
+<button type="{{ $type }}" {{ $attributes->merge(['class' => 'bg-mist rounded-sm font-sans-bold font-bold flex text-navy uppercase pt-10 pb-14 px-20 hover:bg-white transition-all']) }}>
   {{ $slot}}
 </button>
