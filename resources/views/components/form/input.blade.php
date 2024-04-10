3592dc586ee5a8fe2645669170de97bacf58ff62
Marcel Stadelmann <m@marceli.to>
2024-04-10 10:42:11 +0200

    wip

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -4,11 +4,11 @@
     type="{{ $type }}" 
     wire:model="{{ $name }}" 
     placeholder="{{ $placeholder }}" 
-    class="bg-white rounded-sm py-6 pt-6 pb-9 w-full ring-0 focus:ring-0 border-none font-sans-regular text-mist placeholder:text-steel">
+    class="bg-white rounded-sm py-6 pt-6 pb-9 w-full ring-0 focus:ring-0 border-none font-sans-regular text-black placeholder:text-steel">
 @else
   <input 
     type="{{ $type }}" 
     name="{{ $name }}" 
     placeholder="{{ $placeholder }}" 
-    class="bg-white rounded-sm py-6 pt-6 pb-9 w-full ring-0 focus:ring-0 border-none font-sans-regular text-mist placeholder:text-steel">
+    class="bg-white rounded-sm py-6 pt-6 pb-9 w-full ring-0 focus:ring-0 border-none font-sans-regular text-black placeholder:text-steel">
 @endif
