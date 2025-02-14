438b7702942f6470d9a75cf8570b6d95eb144c8e
Marcel Stadelmann <m@marceli.to>
2025-02-14 10:16:55 +0100

    wip

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -24,6 +24,13 @@
 </footer>
 @livewireScripts
 @vite('resources/js/app.js')
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-Q6S1X1JPL6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-Q6S1X1JPL6');
+</script>
 </body>
 </html>
 <!-- made with ❤ by stoz.ch & marceli.to -->
